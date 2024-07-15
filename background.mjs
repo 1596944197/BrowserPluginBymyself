14a764c616ad4516737e8deaeca915ea6485cefd
MEI <1596944197@qq.com>
2024-07-15 16:16:24 +0800

    fix: 修复通知框直接消失的问题

--- background.mjs
+++ background.mjs
@@ -13,7 +13,7 @@ chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
       (id) => {
         setTimeout(() => {
           chrome.notifications.clear(id);
-        });
+        }, 1000);
         sendResponse({ status: "success", message: "调整速度成功" });
       }
     );
@@ -52,43 +52,4 @@ chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
 async function getAllTabs() {
   let tabs = await chrome.tabs.query({});
   return tabs;
-}
-
-function handleGetLocalStorage(req, sender, sendResponse) {
-  // 调用通知接口
-  chrome.notifications.create(
-    undefined,
-    {
-      type: "basic",
-      iconUrl: "icon.png",
-      title: req.data.title,
-      message: req.data.message,
-      silent: true,
-    },
-    (id) => {
-      setTimeout(() => {
-        chrome.notifications.clear(id);
-      });
-      sendResponse({ status: "success", message: "调整速度成功" });
-    }
-  );
-  return true;
-}
-
-function handleSwitchTab(req, sender, sendResponse) {
-  (async () => {
-    const tabs = await getAllTabs();
-    const currentTabIndex = tabs.findIndex((tab) => tab.active);
-    // req.data 是 next or prev 根据这个参数来决定是否切换到下一个tab还是上一个tab
-    let nextTabIndex = currentTabIndex + (req.data === "next" ? 1 : -1);
-    if (nextTabIndex < 0) {
-      nextTabIndex = tabs.length - 1;
-    } else if (nextTabIndex >= tabs.length) {
-      nextTabIndex = 0;
-    }
-    chrome.tabs.update(tabs[nextTabIndex].id, { active: true });
-
-    sendResponse({ status: "success", data: true });
-  })();
-  return true;
 }
