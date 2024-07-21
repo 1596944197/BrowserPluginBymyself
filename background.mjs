c99451d9c10b0d6e701279b27d1a2ff669d62497
MEI <1596944197@qq.com>
2024-07-21 14:05:14 +0800

    fix: 删除无用的content脚本代码

--- background.mjs
+++ background.mjs
@@ -19,43 +19,7 @@ chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
     );
     return true;
   }
-  if (req.type === "SWITCH_TAB") {
-    (async () => {
-      const tabs = await getAllTabs();
-      const currentTabIndex = tabs.findIndex(
-        (tab) => tab.active && tab.url === req.url
-      );
-      // req.data 是 next or prev 根据这个参数来决定是否切换到下一个tab还是上一个tab
-      let nextTabIndex =
-        req.data === "next" ? currentTabIndex + 1 : currentTabIndex - 1;
-      if (nextTabIndex < 0) {
-        nextTabIndex = tabs.length - 1;
-      } else if (nextTabIndex > tabs.length - 1) {
-        nextTabIndex = 0;
-      }
-
-      // 当前的tab设为不激活
-      tabs.at(nextTabIndex)?.id &&
-        chrome.tabs.update(tabs.at(nextTabIndex).id, {
-          active: true,
-          highlighted: true,
-        });
-      tabs.at(currentTabIndex)?.id &&
-        chrome.tabs.update(tabs.at(currentTabIndex).id, { active: false });
-
-      sendResponse({
-        status: "success",
-        data: { tabs, nextTabIndex, currentTabIndex },
-      });
-    })();
-    return true;
-  }
 });
-
-async function getAllTabs() {
-  let tabs = await chrome.tabs.query({});
-  return tabs;
-}
 
 chrome.commands.onCommand.addListener((command) => {
   if (command === "switch-left-tab") {
