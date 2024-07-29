b600a317a763bfa871c705ee309dbc7dc13dd6d7
MEI <1596944197@qq.com>
2024-07-29 17:59:09 +0800

    feat: 增加缓入缓出效果初稿(不确定是否最终方案)

--- background.mjs
+++ background.mjs
@@ -40,31 +40,74 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
   }
 });
 
+// 用一个队列来存储通知
+const notificationQueue = new Proxy([], {
+  set(target, property, value, receiver) {
+    // 触发默认的行为以确保值被正确设置
+    Reflect.set(target, property, value, receiver);
+
+    // 如果队列中新增了一个元素，并且当前没有正在处理的通知，则开始处理队列
+    if (property == target.length - 1 && !notificationQueue.isProcessing) {
+      console.log(235235);
+      processQueue();
+    }
+    return true;
+  },
+});
+
+// 标志当前是否有通知处理函数在执行
+notificationQueue.isProcessing = false;
+
 /**
+ * 处理队列中的下一个通知
+ */
+async function processQueue() {
+  if (notificationQueue.length === 0) {
+    notificationQueue.isProcessing = false;
+    return;
+  }
+
+  notificationQueue.isProcessing = true;
+  const handler = notificationQueue.shift();
+  await handler?.();
+  processQueue();
+}
+
+/**
+ * 处理通知请求
  *
  * @param {*} req
  * @param {Function} sendResponse
  */
 function handleNotification(req, sendResponse) {
   // 调用通知接口
-  chrome.notifications.create(
-    undefined,
-    {
-      type: "basic",
-      iconUrl: "icon.png",
-      title: req.data.title,
-      message: req.data.message,
-      silent: true,
-    },
-    (id) => {
-      setTimeout(() => {
-        chrome.notifications.clear(id);
-      }, 1000);
-      sendResponse({ status: "success", message: "调整速度成功" });
-    }
-  );
-}
+  const notificationHandler = async () => {
+    return new Promise((resolve) => {
+      chrome.notifications.create(
+        undefined,
+        {
+          type: "basic",
+          iconUrl: "icon.png",
+          title: req.data.title,
+          message: req.data.message,
+          silent: true,
+        },
+        (id) => {
+          setTimeout(() => {
+            chrome.notifications.clear(id);
+            setTimeout(() => {
+              resolve();
+            }, 400);
+          }, 800);
+          sendResponse({ status: "success", message: "调整速度成功" });
+        }
+      );
+    });
+  };
 
+  // 将通知处理函数添加到队列
+  notificationQueue.push(notificationHandler);
+}
 function switchTab(direction) {
   chrome.tabs.query({ currentWindow: true }, (tabs) => {
     chrome.tabs.query({ active: true, currentWindow: true }, (activeTabs) => {
